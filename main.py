b12f18b670c0be02fa31c16adb36a834daaf8c9c
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-10-09 22:14:10 +0200

    feat: add basic game window setup

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import pygame
 from constants import *
 
 def main() -> None:
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
