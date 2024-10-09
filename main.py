4ad988e06d429594d98a7ef7131207c76d3e5120
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-10-09 22:11:16 +0200

    feat: add constants.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from constants import *
+
 def main() -> None:
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
