19f8cc44076df2ccb47f0fa64f62fce4e5ba4889
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-10-09 22:15:58 +0200

    feat: add an fps limiter

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main() -> None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main() -> None:
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
