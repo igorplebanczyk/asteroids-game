864c6aa2801a28d63dd210a75f83dbb804d11dde
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-12-14 18:42:35 +0100

    feat: remove window frame

--- src/game.py
+++ src/game.py
@@ -12,7 +12,7 @@ class Game:
     def __init__(self) -> None:
         pygame.init()
 
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.NOFRAME)
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, FONT_SIZE)
 
